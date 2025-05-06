774840012a0cadfd244c572089f8fb9d0ca6fae4
JoanJP <panggestu3214@gmail.com>
2025-05-06 17:01:51 +0700

    06062025 Update

--- db/data-model.cds
+++ db/data-model.cds
@@ -1,12 +1,24 @@
-using {cuid} from '@sap/cds/common';
+using {
+    cuid,
+    managed
+} from '@sap/cds/common';
 
 namespace my.bookshop;
 
 entity Books : cuid {
-    title : String;
-    stock : Integer;
+    title  : String;
+    author : Association to Authors;
+    stock  : Integer;
 }
 
 entity Authors : cuid {
-    name : String;
+    name  : String;
+    books : Association to many Books
+                on books.author = $self;
+}
+
+entity Orders : managed {
+    key ID     : UUID;
+        book   : Association to Books;
+        amount : Integer;
 }
---
+++ srv/cat-service.js
@@ -0,0 +1,29 @@
+const cds = require("@sap/cds");
+module.exports = async function () {
+  const db = await cds.connect.to("db"); // connect to database service
+  const { Books } = db.entities; // get reflected definitions
+
+  // Create order report
+  this.before("CREATE", "Orders", async (req) => {
+    const order = req.data;
+    if (!order.amount || order.amount <= 0)
+      return req.error(400, "Order at least 1 book!");
+    const trx = cds.transaction(req);
+    const affectedRows = await trx.run(
+      UPDATE(Books)
+        .set({ stock: { "-=": order.amount } })
+        .where({ stock: { ">=": order.amount }, /*and*/ ID: order.book_ID })
+    );
+    if (affectedRows === 0) req.error(409, "Stock is not available, sorry!");
+  });
+
+  this.before("DELETE", "Orders", async (req) => {
+    const trx = cds.transaction(req);
+    const orderStock = await trx.run();
+  });
+
+  // Add some discount for overstocked books
+  this.after("READ", "Books", (each) => {
+    if (each.stock > 111) each.title += ` -- 11% discount!`;
+  });
+};
